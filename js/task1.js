5935707df78e6cf1ee40b14f501678c72bc6106e
Donskyi <132575708+Donskyi@users.noreply.github.com>
2024-03-27 11:49:14 +0200

    Add files via upload
    Homework JS

---
+++ js/task1.js
@@ -0,0 +1,24 @@
+//* Завдання 1
+// Дано масив чисел. Потрібно визначити, чи є у ньому хоча б одне від'ємне число. Якщо є, то повернути масив, в якому кожен елемент масиву буде додатнім числом.
+
+// Вхідні дані: масив чисел.
+
+// Вихідні дані: масив додатніх чисел або повідомлення "No negative numbers found".
+
+// Приклад вхідних даних:
+// let arr = [3, -2, 5, 6, -8, 2];
+
+
+let arr = [3, -2, 5, 6, -8, 2];
+
+function removeAllNegatives(arr) {
+    if (arr.some(num => num < 0)) {
+        return arr.map(num => Math.abs(num));
+    } else {
+        return "No negative numbers found";
+    }
+}
+
+
+console.log(removeAllNegatives(arr));
+
